bde95a13b46e43db28a85e3529e4cc88c97f465a
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-04-25 21:05:22 -0700

    install gulp-sass-globe, gulp-source-maps, gulp-plumber and gulp-notify

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sassGlob = require('gulp-sass-glob');
+var sourcemaps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
 
 gulp.task('sass', function() {
   return gulp
     .src('./sass/**/*.scss')
+    .pipe(
+      plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
+    )
+    .pipe(sourcemaps.init())
+    .pipe(sassGlob())
     .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./css'));
 });
 
 gulp.task('sass:watch', function() {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  // https://stackoverflow.com/questions/39665773/gulp-error-watch-task-has-to-be-a-function
+  gulp.watch('./sass/**/*.scss', gulp.series('sass'));
 });
